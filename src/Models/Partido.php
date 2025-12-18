9e9df421e71322eb9e43ec211ea55af55126fdc3
Felix Cortez <fericor@hotmail.com>
2025-12-19 00:45:49 +0100

    12

--- src/Models/Partido.php
+++ src/Models/Partido.php
@@ -4,6 +4,7 @@ namespace PadelClub\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use PadelClub\Models\ResultadoPartido;
 
 class Partido extends Model
 {
