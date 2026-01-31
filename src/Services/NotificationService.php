1489cefc12cfd0b33eff84e04b432b89e050df68
Felix Cortez <fericor@hotmail.com>
2026-01-31 15:51:09 +0100

    12

--- src/Services/NotificationService.php
+++ src/Services/NotificationService.php
@@ -112,6 +112,8 @@ class NotificationService
 
     public function sendPlayerJoinedNotification($partido, $jugador, $organizadorEmail)
     {
+
+    echo "VALOR: ";
         $data = [
             'player_name'       => $jugador->nombre ?? $jugador->username,
             'organizer_name'    => $organizadorEmail, // O nombre del organizador
